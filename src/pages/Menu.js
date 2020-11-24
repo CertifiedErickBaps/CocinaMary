2d37810c0c2377fb94e3029ce9c7faf0c91f20c2
Ericksad01 <erickburn01@hotmail.com>
2020-11-24 14:01:04 -0600

    Menu items fixed

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,23 +1,8 @@
 import React from 'react';
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css";
-
-
 import iconRate from "../images/rate.svg";
 import {firestore} from '../firebase';
-
-const settings = {
-  dots: true,
-  lazyLoad: true,
-  infinite: true,
-  speed: 500,
-  cssEase: "linear",
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  className: "center",
-  centerMode: true,
-};
 
 class ItemFood extends React.Component {
   constructor(props) {
@@ -25,9 +10,7 @@ class ItemFood extends React.Component {
     this.state = {};
   }
 
-
   render() {
-
     let imagen = this.props.imagen;
     let precio = this.props.precio;
     let titulo = this.props.titulo;
@@ -39,20 +22,20 @@ class ItemFood extends React.Component {
           <div className="food">
             <img src={imagen} alt="Food"/>
           </div>
-
           <div className="information-item">
             <span>$ {precio} MXN </span>
             <img className="rate" src={iconRate} alt="Rate"/>
             <span>{titulo}</span>
-            <button onClick={() => this.props.handleCarrito({titulo: titulo, precio: precio, imagen: imagen, piezas: 1})}
-                    className="waves-effect waves-light btn">Agregar a carrito
+            <button
+              onClick={() => this.props.handleCarrito({titulo: titulo, precio: precio, imagen: imagen, piezas: 1})}
+              className="waves-effect waves-light btn">Agregar a carrito
             </button>
           </div>
         </div>
       </>
     )
   }
-};
+}
 
 class Menu extends React.Component {
   constructor(props) {
@@ -65,16 +48,16 @@ class Menu extends React.Component {
   }
 
   showSection = (name) => {
-    console.log(name.target.id);
+    // console.log(name.target.id);
     switch (name.target.id) {
       case "clasico":
-        this.setState({sectionClasico: !this.state.sectionClasico});
+        this.setState({sectionClasico: !this.state.sectionClasico, sectionTemporada: false, sectionAdicional: false});
         break;
       case "temporada":
-        this.setState({sectionTemporada: !this.state.sectionTemporada});
+        this.setState({sectionTemporada: !this.state.sectionTemporada, sectionClasico: false, sectionAdicional: false});
         break;
       case "adicionales":
-        this.setState({sectionAdicional: !this.state.sectionAdicional});
+        this.setState({sectionAdicional: !this.state.sectionAdicional, sectionClasico: false, sectionTemporada: false});
         break;
       default:
         return null;
@@ -83,30 +66,52 @@ class Menu extends React.Component {
 
   componentDidMount() {
     firestore.collection('paquetes').get().then(snapshot => {
-      let itemsComidita = [];
+      let itemsClasico = [];
+      let itemsTemporada = [];
+      let itemsAdicional = [];
       snapshot.forEach(doc => {
         let info = doc.data();
-        itemsComidita.push(<ItemFood titulo={info.titulo} precio={info.precio} imagen={info.imagen}
-                                     handleCarrito={this.props.handleCarrito}/>);
+        // console.log(info)
+        switch (info.tipo) {
+          case("Clasico"):
+            itemsClasico.push(<ItemFood titulo={info.titulo} precio={info.precio} imagen={info.imagen}
+                                        handleCarrito={this.props.handleCarrito}/>);
+            break;
+          case("Temporada"):
+            itemsTemporada.push(<ItemFood titulo={info.titulo} precio={info.precio} imagen={info.imagen}
+                                          handleCarrito={this.props.handleCarrito}/>);
+            break;
+          case("Adicional"):
+            itemsAdicional.push(<ItemFood titulo={info.titulo} precio={info.precio} imagen={info.imagen}
+                                          handleCarrito={this.props.handleCarrito}/>);
+            break;
+          default:
+            return null;
+        }
       });
-      this.setState({itemsComidita});
+      this.setState({itemsClasico, itemsTemporada, itemsAdicional});
     });
   }
 
   render() {
-    let {itemsComidita} = this.state;
-
-    // let menu;
-    // if (sectionClasico) {
-    //   menu = <div>Holaaa</div>;
-    // } else if (sectionTemporada) {
-    //   menu = itemsComidita;
-    // } else if (sectionAdicional) {
-    //   menu = itemsComidita;
-    // }
-
-
-    // console.log(this.state.itemsComidita);
+    let {itemsClasico, itemsTemporada, itemsAdicional} = this.state;
+    console.log(this.state.sectionClasico);
+    console.log(this.state.sectionTemporada);
+    console.log(this.state.sectionAdicional);
+    let showMenu;
+    if (this.state.sectionClasico) {
+      showMenu = null;
+      showMenu = itemsClasico;
+    }
+    if (this.state.sectionTemporada) {
+      showMenu = null;
+      showMenu = itemsTemporada;
+    }
+    if (this.state.sectionAdicional) {
+      showMenu = null;
+      showMenu = itemsAdicional;
+    }
+    console.log(showMenu);
 
     return (<>
       <div className="menu" id="menu">
@@ -122,27 +127,24 @@ class Menu extends React.Component {
             <span id="temporada" className="" onClick={e => this.showSection(e)}>Menú de Temporada</span>
             <span id="adicionales" className="" onClick={e => this.showSection(e)}>Adicionales</span>
           </div>
-          {(this.state.sectionClasico) && (this.state.sectionTemporada === false) && (this.state.sectionAdicional === false) && (
-            <div className="item">
-              {itemsComidita}
-            </div>
-          )}
-          {(this.state.sectionTemporada) && (this.state.sectionClasico === false) && (this.state.sectionAdicional === false) && (
-            <div className="item">
-              {itemsComidita}
-            </div>
-          )}
-          {(this.state.sectionAdicional) && (this.state.sectionTemporada === false) && (this.state.sectionClasico === false) && (
-            <div className="item">
-              {itemsComidita}
-            </div>
-          )}
-          {/*<div className="item">*/}
-          {/*  <Slider {...settings}>*/}
-          {/*    {itemsComidita}*/}
-          {/*  </Slider>*/}
-          {/*</div>*/}
-
+          <div className="item">
+            {showMenu}
+          </div>
+          {/*{(this.state.sectionClasico) && (this.state.sectionTemporada === false) && (this.state.sectionAdicional === false) && (*/}
+          {/*  <div className="item">*/}
+          {/*    {itemsClasico}*/}
+          {/*  </div>*/}
+          {/*)}*/}
+          {/*{(this.state.sectionTemporada) && (this.state.sectionClasico === false) && (this.state.sectionAdicional === false) && (*/}
+          {/*  <div className="item">*/}
+          {/*    {itemsTemporada}*/}
+          {/*  </div>*/}
+          {/*)}*/}
+          {/*{(this.state.sectionAdicional) && (this.state.sectionTemporada === false) && (this.state.sectionClasico === false) && (*/}
+          {/*  <div className="item">*/}
+          {/*    {itemsAdicional}*/}
+          {/*  </div>*/}
+          {/*)}*/}
         </div>
       </div>
     </>)
